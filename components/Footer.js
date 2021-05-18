8bf4968f22a70eac5f86710b10356e08ee168c81
Ishav bhatt <ishavbhattkhaniyara.com>
2021-05-18 10:03:31 +0530

    changes

--- components/Footer.js
+++ components/Footer.js
@@ -3,111 +3,88 @@ import Link from "next/link";
 
 function Footer() {
   return (
-    <section className="lg:py-10 lg:px-28 md:p-10 px-5 py-10 bg-ftr-bg">
-      <div className="flex flex-wrap lg:justify-between -mx-1 lg:-mx-4">
-        <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-2/5">
-          <h2 className="text-white text-2xl mb-5">
-        Contact Us
-          </h2>
-          {[
-            {
-              icon: "/icons/pin.png",
-              text: "Dharmashal, Himachal Pradesh, India",
-            },
-            {
-              icon: "/icons/mail.png",
-              text: "ishavbhattkhaniyara@gmail.com",
-            },
-            {
-              icon: "/icons/smartphone.png",
-              text: "9418249045",
-            },
-          ].map((item, index) => (
-            <div className="flex my-5" key={index}>
-              <Image src={item.icon} width={30} height={30} />
-                <a className="text-custom-grey ml-2 text-base font-thin align-center">
+    <footer className="relative bg-ftr-bg lg:py-10 lg:px-28 px-5 py-10">
+      <div className="container mx-auto">
+        <div className="flex justify-between flex-wrap">
+          <div className="my-1 w-full md:w-1/2 lg:w-2/5">
+          <span className="block text-gray-300 text-xl font-medium mb-2">Let's keep in touch!
+</span>
+            {[
+              {
+                icon: "/icons/pin.png",
+                text: "Dharmashal, Himachal Pradesh, India",
+              },
+              {
+                icon: "/icons/envelope.png",
+                text: "ishavbhattkhaniyara@gmail.com",
+              },
+              {
+                icon: "/icons/smartphone.png",
+                text: "9418249045",
+              },
+            ].map((item, index) => (
+              <div className="flex my-5" key={index}>
+                <Image src={item.icon} width={25} height={20} />
+                <a className="text-custom-grey ml-2 text-base font-normal align-center">
                   {item.text}
                 </a>
-            </div>
-          ))}{" "}
-        </div>
-        <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/5">
-          <h2 className="text-white text-2xl mb-5">
-            Quick Links
-          </h2>
-          {[
+              </div>
+            ))}
+          </div>
+          <div className="w-full lg:w-6/12">
+            <div className="flex flex-wrap items-top mb-6">
+              <div className="w-full lg:w-4/12 px-4 ml-auto">
+                <span className="block text-gray-300 text-xl font-medium mb-2">
+                  Useful Links
+                </span>
+                {[
             {
               text: "Home",
-              link: "/",
+              link: "/"
             },
             {
               text: "About Us",
-              link: "/",
+              link: "/"
             },
             {
               text: "Services",
-              link: "/",
+              link: "/"
             },
             {
-              text: "Trekking Packages",
-              link: "/",
-            },
-            {
-              text: "Contact Us",
-              link: "/",
-            },
+              text: "Projects",
+              link: "/"
+            }
           ].map((item, index) => (
-            <div className="flex text-center my-3" key={index}>
-              <Image src="/icons/next.png" width={20} height={20} />
-              <Link href={item.link}>
-                <a className="text-custom-grey text-base font-thin align-center hover:text-custom-orange">
-                  {item.text}
-                </a>
-              </Link>
+                <ul className="mb-2 list-unstyled">
+                <Link href={item.link}>
+                  <a className="mb-3 text-custom-grey text-base font-normal hover:text-gray-400">{item.text}</a>
+                </Link>
+              </ul>
+              ))}
+
+              </div>
+              <div className="w-full lg:w-4/12 px-4">
+                <span className="block text-gray-300 text-xl font-medium mb-2">
+                  Follow Us On
+                </span>
+                <div className="">
+          <a href="https://www.facebook.com/" target="blank" className="pr-2 hover:opacity-75"><Image src="/icons/facebook.png" width={35} height={35} /></a>
+          <a href="https://www.instagram.com/" target="blank" className=" hover:opacity-75"><Image src="/icons/instagram.png" width={35} height={35} /></a>
+          </div>
+              </div>
             </div>
-          ))}
+          </div>
         </div>
-        <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/5">
-          <h1 className="text-white text-2xl mb-5">
-            Best Packages
-          </h1>
-          {[
-            {
-              text: "Kedarnath Trek",
-              link: "/UkTrekking",
-            },
-            {
-              text: "Dharamshala Tour",
-              link: "/NorthIndiaTour",
-            },
-            {
-              text: "Vaishnu Devi Tour",
-              link: "/SaktipithTour",
-            },
-            {
-              text: "Triund Trek",
-              link: "/HimachalTrekking",
-            },
-            {
-              text: "Mini Swiss Tour",
-              link: "/NorthIndiaTour",
-            },
-          ].map((item, index) => (
-            <div className="flex my-3" key={index}>
-              <Image src="/mail.png" width={20} height={20} />
-              <Link href={item.link}>
-                <a className="text-custom-grey text-base font-thin align-center hover:text-custom-orange">
-                  {item.text}
-                </a>
-              </Link>
+        <hr className="my-6 border-gray-400" />
+        <div className="flex flex-wrap items-center md:justify-between justify-center">
+          <div className="w-full md:w-4/12 px-4 mx-auto text-center">
+            <div className="text-sm text-gray-600 font-semibold py-1">
+              © Mud House Contractor, All Right Reserved.
             </div>
-          ))}
+          </div>
         </div>
       </div>
-      {/* <p className="text-center mt-4 bg-copyright-bg text-custom-grey cursor-pointer font-light text-base border-b-50">
-        © Dev Bhumi holidays. All rights reserved.
-      </p> */}
-    </section>
+    </footer>
   );
 }
 export default Footer;
--- pages/Projects.js
+++ pages/Projects.js
@@ -3,35 +3,38 @@ import Link from "next/link";
 
 function Projects() {
   return (
-    <section className="services pt-20 lg:px-28 md:px-28 px-5">
+    <section className="services pt-20 lg:px-28 px-5">
       <h1 className="title">Our Projects</h1>
       <p className="text-t-c-grey text-center">Looking for a Mud house contractor? You can rest assured that you are working with the most creative, experienced & qualified technical experts in the mud house construction industry. We offer full range of services, from site inspection and architectural design through to testing and analysis of your structure.</p>
       <div className="md:flex justify-between flex-wrap">
         {[
           {
             picture: "/construction.jpg",
+            heading: "Mud House"
           },
           {
             picture: "/construction.jpg",
+            heading: "Mud House"
           },
           
           {
             picture: "/construction.jpg",
-          },
-          {
-            picture: "/construction.jpg",
+            heading: "Mud House"
           }
         ].map((item, index) => (
-          <div className="border border-b-r-color rounded bg-white overflow-hidden md:w-2/5 lg:w-1/5 my-10 m-h-72 hover:border-none hover:shadow-2xl" key={index}>
-            <div className="hover:opacity-75">
-              <Image
-                src={item.picture}
-                width={800}
-                height={400}
-                layout="responsive"
-              />
+          <div className="container rounded relative overflow-hidden md:w-1/4 lg:w-30p my-5" key={index}>
+            <img src={item.picture} alt="Avatar" className="block w-full h-72" />
+            <div className="overlay">
+              <div className="text">{item.heading}</div>
             </div>
           </div>
+
+          // <div className="rounded relative overflow-hidden md:w-2/5 lg:w-1/5 my-5" key={index}>
+          //   <div className="">
+          //     <img className="h-56 w-full" src={item.picture} alt="" />
+          //     <h1 className="text-custom-heading text-2xl pt-2 font-ghre">{item.heading}</h1>
+          //   </div>
+          // </div>
         ))}
       </div>
     </section>
